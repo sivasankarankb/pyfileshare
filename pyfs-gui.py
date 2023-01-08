9667af30a3ea00754e02596f0768f96244a7bcdd
Sivasankaran K B <kbsiva7@gmail.com>
2023-01-08 20:41:22 +0530

    Add about screen to client

--- pyfs-gui.py
+++ pyfs-gui.py
@@ -11,6 +11,46 @@ from tkinter import ttk
 
 from pyfs_client import PyFSClient
 
+class AboutBox:
+    def __init__(self, master):
+        win = tk.Toplevel(master)
+        win.title('About')
+        win.transient(master)
+
+        frame = ttk.Frame(win)
+        frame.grid(padx=16, pady=16)
+
+        win.columnconfigure(0, weight=1)
+        win.rowconfigure(0, weight=1)
+        
+        l1 = ttk.Label(frame, text='pyfileshare')
+        l1.grid(pady=(0,8))
+        
+        l2 = ttk.Label(frame, text='Version v0.1')
+        l2.grid(pady=(0,8))
+
+        l3 = ttk.Label(frame, text='https://github.com/sivasankarankb/pyfileshare')
+        l3.grid(pady=(0,8))
+
+        lcopy = ttk.Label(frame, text='Copyright (C) 2020-2023 Sivasankaran K B')
+        lcopy.grid(pady=(0,20))
+
+        license_txt ='''\
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation.'''
+
+        llicense = ttk.Label(frame, text=license_txt)
+        llicense.grid(pady=(0,20))
+
+        l4 = ttk.Label(frame, text='-- Contributors --')
+        l4.grid(pady=(0,20))
+
+        ll = ttk.Label(frame, text='Icons: Sudharsanan K B')
+        ll.grid(pady=(0,8))
+
+        frame.columnconfigure(0, weight=1)
+
 class Application:
     def __create_treeview(self, master, columns):
         treeview = ttk.Treeview(master=master)
@@ -280,7 +320,7 @@ class Application:
         help_menu = tk.Menu(self.__tk, tearoff=False)
         
         help_menu.add(
-            tk.COMMAND, label='About'
+            tk.COMMAND, label='About', command=self.__about_menu
         )
         
         self.__menubar.add(
@@ -712,6 +752,9 @@ class Application:
         self.__pref_cancel_button.grid(
             row=2, column=4, padx=(0,8), pady=(0,8), sticky=tk.W
         )
+
+    def __about_menu(self):
+        about_box = AboutBox(self.__tk)
 
     def __pref_menu_download_dest_select(self):
         dir = tk_filedialog.askdirectory()
