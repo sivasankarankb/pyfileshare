e0f88efa0836df2658526c04bb661a775666fec4
Sivasankaran K B <kbsiva7@gmail.com>
2023-01-08 21:31:37 +0530

    Set about box version to 0.1.1

--- pyfs-gui.py
+++ pyfs-gui.py
@@ -26,7 +26,7 @@ class AboutBox:
         l1 = ttk.Label(frame, text='pyfileshare')
         l1.grid(pady=(0,8))
         
-        l2 = ttk.Label(frame, text='Version v0.1')
+        l2 = ttk.Label(frame, text='Version v0.1.1')
         l2.grid(pady=(0,8))
 
         l3 = ttk.Label(frame, text='https://github.com/sivasankarankb/pyfileshare')
